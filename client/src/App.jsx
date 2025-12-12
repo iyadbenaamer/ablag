dbc106ead56451ad04c5252a1b48317d2bb84f6e
iyadbenaamer <iyadbenaamer1@gmail.com>
2025-12-12 08:34:49 +0200

    update API_BASE to use production URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import LogoIcon from "./assets/icon.svg?react";
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
+const API_BASE = "https://ablag.onrender.com";
 
 export default function App() {
   const [text, setText] = useState("");
